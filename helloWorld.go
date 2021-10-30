5a0b76e80e729d77b35a7cfb4741a4d17f32d987
Dexter Miller <36194823+dextercarlmiller@users.noreply.github.com>
2021-10-30 17:49:08 -0400

    Add App package

--- helloWorld.go
+++ helloWorld.go
@@ -1,7 +1,26 @@
 package main
+/*
+For this app to run you need to install fyne.io/fyne
+This can be done with the below command
+go get fyne.io/fyne
 
-import "fmt"
+Then compile and run
+*/
+import (
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/widget"
+	"fmt"
+}
 
 func main() {
 	fmt.Println("Hello, World!")
+	a := app.New()
+
+	w := a.NewWindows("Hello World")
+	w.Setcontent(widget.NewVBox(
+		widget.NewLabel("Hello World"),
+		widget.NewButton("Quit", func(){a.Quit()
+		}),
+	))
+w.ShowAndRun()
 }
